01fdf2dad689e8bc6849151bf355485a68923bce
xtsebas <thiagohugo2014@gmail.com>
2024-11-16 14:57:58 -0600

    first commit

---
+++ target/debug/SpaceTravel.d
@@ -0,0 +1,1 @@
+C:\Users\Sebastian\projects\SpaceTravel\target\debug\SpaceTravel.exe: C:\Users\Sebastian\projects\SpaceTravel\src\main.rs
---
+++ target/debug/deps/SpaceTravel.d
@@ -0,0 +1,5 @@
+C:\Users\Sebastian\projects\SpaceTravel\target\debug\deps\SpaceTravel.exe: src/main.rs
+
+C:\Users\Sebastian\projects\SpaceTravel\target\debug\deps\SpaceTravel.d: src/main.rs
+
+src/main.rs:
